ff0dfb91565f2e8d95b4f8eb684b75eb801d2209
Toby Flemming <tobyf93@hotmail.com>
2016-12-15 23:57:37 +1100

    Grid showing images in chunks of 3

--- app/src/components/Grid.js
+++ app/src/components/Grid.js
@@ -1,34 +1,65 @@
 import React, { Component, PropTypes } from 'react';
 
-// {
-//   images.map((image, i) => (
-//     <button onClick={() => showGallery(i)}>
-//       <img
-//         key={i}
-//         role="presentation"
-//         src={image.url}
-//       />
-//     </button>
-//   ))
-// }
-
 class Grid extends Component {
-  componentDidMount() {
-    this.div.style.height = `${this.div.offsetWidth}px`;
+  constructor() {
+    super();
+    this.divs = [];
   }
 
-  render() {
-    const style = {
+  componentDidUpdate() {
+    for (let i = 0; i < this.divs.length; i += 1) {
+      const div = this.divs[i];
+      div.style.height = `${div.offsetWidth}px`;
+    }
+  }
+
+  getGrid() {
+    const grid = [];
+    const chunkSize = 3;
+    const baseStyle = {
       flex: 1,
-      border: '1px solid black',
+      border: '5px solid black',
       margin: '10px',
     };
 
+    for (let i = 0; i < this.props.images.length; i += chunkSize) {
+      const imageHolders = this.props.images.slice(i, i + chunkSize).map((image, j) => {
+        const style = {
+          ...baseStyle,
+          backgroundImage: `url('${image.url}')`,
+        };
+        const key = this.props.images.indexOf(image);
+
+        if (j === 0) {
+          return (
+            <div
+              key={key}
+              ref={(ref) => { this.divs.push(ref); }}
+              style={style}
+            />
+          );
+        }
+
+        return <div key={key} style={style} />;
+      });
+
+      while (imageHolders.length !== chunkSize) {
+        imageHolders.push(<div key={-imageHolders.length} style={baseStyle} />);
+      }
+
+      grid.push(
+        <div key={i} style={{ display: 'flex' }}>
+          {imageHolders}
+        </div>);
+    }
+
+    return grid;
+  }
+
+  render() {
     return (
-      <div style={{ display: 'flex' }}>
-        <div ref={(ref) => { this.div = ref; }} style={style} />
-        <div style={style} />
-        <div style={style} />
+      <div>
+        {this.getGrid()}
       </div>
     );
   }

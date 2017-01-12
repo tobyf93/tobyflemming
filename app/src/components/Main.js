de816314eb9229999d5406bf6e1dede206582aca
Toby Flemming <tobyf93@hotmail.com>
2017-01-12 13:24:55 +1100

    Skeleton menu in place with dumby items

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Gallery from './Gallery';
@@ -13,19 +13,45 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <div className="header">Toby Flemming</div>
-        <Gallery
-          ref={(ref) => { this.gallery = ref; }}
-          images={this.props.images}
-        />
-        <Grid
-          images={this.props.images}
-          showGallery={i => this.gallery.show(i)}
-        />
+        <div className="pageWrapper">
+          <div className="header">Toby Flemming</div>
+          <Gallery
+            ref={(ref) => { this.gallery = ref; }}
+            images={this.props.images}
+          />
+          <Grid
+            images={this.props.images}
+            showGallery={i => this.gallery.show(i)}
+          />
+        </div>
+        <div className="menu">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div>
       </div>
     );
   }
 }
+
+Main.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  fetchFeed: PropTypes.func.isRequired,
+};
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
